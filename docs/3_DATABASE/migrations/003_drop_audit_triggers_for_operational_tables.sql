d73c5c6215cfb761345f9af7de3d04a791bcdf9d
Hoang <uznplay@gmail.com>
2026-07-22 12:10:34 +0700

    audit

--- docs/3_DATABASE/migrations/003_drop_audit_triggers_for_operational_tables.sql
+++ docs/3_DATABASE/migrations/003_drop_audit_triggers_for_operational_tables.sql
@@ -4,6 +4,8 @@
 -- Ngày: 2026-07-22
 
 -- Xoá trigger cũ (nếu còn tồn tại)
+DROP TRIGGER IF EXISTS [dbo].[trg_voucher_audit];
+GO
 DROP TRIGGER IF EXISTS [dbo].[trg_order_audit];
 GO
 DROP TRIGGER IF EXISTS [dbo].[trg_payment_audit];
@@ -15,5 +17,5 @@ GO
 DROP TRIGGER IF EXISTS [dbo].[trg_stock_transaction_audit];
 GO
 
-PRINT N'=== Da xoa 5 trigger audit log cho bang nghiep vu thanh cong! ===';
+PRINT N'=== Da xoa 6 trigger audit log cho bang nghiep vu thanh cong! ===';
 GO
