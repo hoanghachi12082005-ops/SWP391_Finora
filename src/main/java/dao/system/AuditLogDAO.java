a691ed564be94a32630f5273178cbd585a6cb970
Hoang <uznplay@gmail.com>
2026-07-22 15:43:31 +0700

    remove line 12/30: String sql = "

--- src/main/java/dao/system/AuditLogDAO.java
+++ src/main/java/dao/system/AuditLogDAO.java
@@ -1,4 +1,3 @@
-        String sql = """
             INSERT INTO audit_log (emp_id, action_name, table_name, record_id, old_data, new_data, created_at)
             VALUES (?, ?, ?, ?, ?, ?, GETDATE())
             """;
